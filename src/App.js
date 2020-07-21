67e7958a465209350450a08caca90709148b3916
HenrikDK2 <Henrik265@outlook.dk>
2020-07-21 12:50:25 +0200

    Initial Files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-size: 3rem;
+  text-align: center;
+`;
+
+function App() {
+  return (
+    <main>
+      <Heading>Recoil Library Test</Heading>
+    </main>
+  );
+}
+
+export default App;
