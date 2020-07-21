a56b262ef6cee7e4aad2da1695a00f803b8a1c8a
HenrikDK2 <Henrik265@outlook.dk>
2020-07-21 16:03:38 +0200

    Et grid container for alle produkter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import Item from "./components/productComponent";
 
 const Heading = styled.h1`
   font-size: 3rem;
   text-align: center;
 `;
 
+const ProductGrid = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 200px);
+  padding: 0;
+  list-style: none;
+  grid-auto-rows: 300px;
+  place-content: center;
+  margin: 200px auto;
+  grid-gap: 20px;
+  max-width: 900px;
+`;
+
 function App() {
   return (
     <main>
       <Heading>Recoil Library Test</Heading>
+      <ProductGrid>
+        <Item title="Produkt" price="50" src="https://via.placeholder.com/350x150" alt="Produkt" />
+        <Item title="Produkt" price="50" src="https://via.placeholder.com/350x150" alt="Produkt" />
+        <Item title="Produkt" price="50" src="https://via.placeholder.com/350x150" alt="Produkt" />
+        <Item title="Produkt" price="50" src="https://via.placeholder.com/350x150" alt="Produkt" />
+        <Item title="Produkt" price="50" src="https://via.placeholder.com/350x150" alt="Produkt" />
+      </ProductGrid>
     </main>
   );
 }
